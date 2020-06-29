181b949c5fe1c4604403b5d4a6aa99b736a25b1f
anton9088 <anton9088-ros@mail.ru>
2020-06-29 22:08:24 +0300

    fix script

--- bin/openapi3/dart-jaguar-petstore.sh
+++ bin/openapi3/dart-jaguar-petstore.sh
@@ -1,29 +1,7 @@
 #!/bin/sh
 
-SCRIPT="$0"
-echo "# START SCRIPT: $SCRIPT"
-
-while [ -h "$SCRIPT" ] ; do
-  ls=`ls -ld "$SCRIPT"`
-  link=`expr "$ls" : '.*-> \(.*\)$'`
-  if expr "$link" : '/.*' > /dev/null; then
-    SCRIPT="$link"
-  else
-    SCRIPT=`dirname "$SCRIPT"`/"$link"
-  fi
-done
-
-if [ ! -d "${APP_DIR}" ]; then
-  APP_DIR=`dirname "$SCRIPT"`/..
-  APP_DIR=`cd "${APP_DIR}"; pwd`
-fi
-
 executable="./modules/openapi-generator-cli/target/openapi-generator-cli.jar"
-
-if [ ! -f "$executable" ]
-then
-  mvn clean package
-fi
+mvn package -DskipTests
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -Xmx1024M -DloggerPath=conf/log4j.properties"
